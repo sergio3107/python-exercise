d78ef164717fccefd200ace56fe82fc9f98b1867
sergio <pillaysergio@gmail.com>
2020-03-19 09:47:36 +0200

    loops

---
+++ cars/cars.ex.py
@@ -0,0 +1,12 @@
+cars=["benz","toyota","nissan","bmw","audi","lexus"]
+bikes=["yamaha","honda","hayabusa","kawasaki","bmw"]
+for thecars in cars:
+    for thebikes in bikes:
+        print(thecars,thebikes)
+for number in range(1,11):
+    print(number)
+for numbers in range(2,22,2):
+    print(numbers)
+else:
+    print("we have completed our exercise")
+
